ab921138fc9a50a71aa34c517a160ff0ceb08163
abdullox0900 <abdullox0900@gmail.com>
2023-12-06 19:54:13 +0500

    python x-py-1 learning variables and date types

--- x-python-1/main.py
+++ x-python-1/main.py
@@ -1,13 +1,66 @@
-# Bu String pythonda => Str 
+# This is a comment
+
+'''
+# This is comment v2
+'''
+
+
+
+# =================== Python Date types ===================
+
+# These are the basic data types in Python.
+# Python is dynamically typed, meaning you don't have to explicitly declare the data type of a variable; it is inferred at runtime. Additionally, Python is an object-oriented language, and everything in Python is an object, including these data types.
+
+# This is a (Str) 
 string = 'Hello World'
 
-# Bu Number pythonda => integer => int
+# This is a (Integer) => (Int)
 number = 15
 
-# Bu Float
+# This is a (Float)
 f_number = 1.56
 
-# Bu Boolean pythonda => Bool
+# This is a (Bool)
 status = False
 
-print(status)
+# This is a (list)
+list_names = ['Ali', 'Muhammad', 'Bek']
+
+#This is a (Dictionary)
+dictionary_variables = {"name": "Me", "age": "18"}
+
+# This is a (None)
+none_variables = None
+
+
+# <=================== Basic Operations ===================> #
+
+
+# <=================== Arithmetic Operations ===================> #
+
+a = 10
+b = 3 
+
+print(a + b)  # (Addition)       print value = 13
+print(a - b)  # (Subtraction)    print value = 7
+print(a * b)  # (Multiplication) print value = 30
+print(a / b)  # (Devision)       print value = 3.3333333333333335
+print(a // b) # (Floor Devision) print value = 3
+print(a % b)  # (Modulus)        print value = 1
+print(a ** b) # (Exponentiation) print value = 1000
+
+
+# <=================== Comparison Operations ===================> #
+
+print(5 > 10)   # Greater than                result = False
+
+print(5 < 10)   # Less than                   result = True
+
+print(10 >= 10) # Greater than or equal to    result = True
+
+print(5 <= 10)  # Less than or equal to       result = True
+
+print(10 != 10) # Not equal to                result = False
+
+print(5 == 5)   # Equal to                    result = True
+ 
---
+++ x-python-1/practise.py
@@ -0,0 +1,8 @@
+what = input('Nima qilmoqchisiz (+, -)')
+number_1 = int(input('X raqam kiriting: '))
+number_2 = int(input('Y raqam kiriting: '))
+
+if what == '+':
+    print(number_1 + number_2)
+elif what == '-':
+    print(number_1 - number_2)
