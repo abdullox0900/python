82ed0b2fb4db0285d6dce1ce14abd86f8b172304
abdullox0900 <abdullox0900@gmail.com>
2023-12-06 09:26:35 +0500

    first commit

---
+++ x-python-1/main.py
@@ -0,0 +1,13 @@
+# Bu String pythonda => Str 
+string = 'Hello World'
+
+# Bu Number pythonda => integer => int
+number = 15
+
+# Bu Float
+f_number = 1.56
+
+# Bu Boolean pythonda => Bool
+status = False
+
+print(status)
