215154a24ae4b940e2cef01b72fcceb9b9a8a5c6
abdullox0900 <abdullox0900@gmail.com>
2023-12-07 12:35:46 +0500

    [day-2] learing python (Special characters) and (f-string)

---
+++ x-python-day-2/main.py
@@ -0,0 +1,62 @@
+# ism = 'Alisher'
+# familiya = 'Mominov'
+
+# print(ism + '  ' + familiya)
+# print('Mening ismim: ' + ism)
+
+# <<============ f-string ============>>
+
+# Ikki (va undan ko'p) matn ko'rinishidagi o'zgaruvchilarni birlashtirish uchun f-string usulidan  f"{matn1} {matn2}" ham foydalansak bo'ladi:
+
+# result = f"{ism} {familiya}"
+
+# print(result)
+
+# (Bu usul yordamida uzun matnlarni ham yasash mumkin:)
+
+# ism = "Alisher"
+# falimiya = "Mominov"
+
+# result = f"Hi, My first name {ism} My last name {falimiya}"
+
+# print(result)
+
+# <<============ Special characters - Mahsus belgilar ============>>
+
+# print('Hello World!')     #Result Hello World
+# print('Hello \tWorld!')   #Result Hello   World
+# print('Hello \nWorld!')   #Result Hello 
+                                # World
+
+# <<============ String Methods - Matin Metodlari ============>>
+
+# first_name = 'Abdulloh'
+# last_name = 'Abdusalomov'
+
+# full_name = f'{first_name} {last_name}'
+
+# str_upper = full_name.upper()   # result (ABDULLOH ABDUSALOMOV)
+# str_lower = full_name.lower()   # result (abdulloh abdusalomov)
+
+# str_text = 'hello world'       
+
+# str_text.title()                # result (hello world)
+
+# str_text2 = 'hello world'       
+
+# str_text2.capitalize()           # result (Hello world)
+
+
+# Metodlarni faqat o'zgaruvchilarga emas, 
+# balki to'g'ridan-to'g'ri matnga ham qo'llash mumkin
+# (aslida o'zgaruvchi ham matnning (yoki boshqa ma'lumotning) manzili xolos)
+
+# print('Hello World'.upper())
+# print('HI WORLD'.lower())
+# print('hello bro'.title())
+# print('alisher aka'.capitalize())
+
+# txt = 'banana'
+
+# print(txt.center(30))
+# print(len(txt))
